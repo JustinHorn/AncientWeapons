e4e72b33a833a4b2d6f516d2467c9ec0d19cb590
y0092888 <ju-horn@web.de>
2020-08-13 17:47:32 +0200

    Co-authored-by: MrKnb <mrknabe2@gmail.com> Co-authored-by: Usamoto <ostratouli@yahoo.de>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import { client } from "./Client";
 
 function App() {
-  const [entries, setEntries] = useState([]);
+  const [entries, setEntries] = useState({});
+
   useEffect(() => {
     client
       .getEntries()
       .then((response) => {
-        console.log(response);
-        setEntries(response.items);
+        // items
+        // 0
+        // fields
+        // name , image {}, description: "...."
+        //image: fields:  file url
+        const value = {
+          name: response.items[0].fields.name,
+          description: response.items[0].fields.description,
+          url: response.items[0].fields.image.fields.file.url,
+        };
+        setEntries(value);
       })
       .catch(console.error);
   }, []);
   return (
     <div className="App">
-      {entries.map((x) => (
-        <div>
-          <h1>{x.name}</h1> <h3>{x.description}</h3>
-        </div>
-      ))}
+      <Sword {...entries} />
+      <Sword
+        name={entries.name}
+        description={entries.description}
+        url={entries.url}
+      />
     </div>
+  );
+}
+
+//...
+function Sword(entries) {
+  return (
+    <>
+      <h1>{entries.name}</h1>
+      <img src={entries.url} alt="sword" />
+      <p>{entries.description}</p>
+    </>
   );
 }
 
