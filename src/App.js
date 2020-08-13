d9697a0425849bc36e7afa1a48ba4931f6e376da
y0092888 <ju-horn@web.de>
2020-08-13 17:01:44 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { client } from "./Client";
 
 function App() {
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((response) => {
+        console.log(response);
+        setEntries(response.items);
+      })
+      .catch(console.error);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {entries.map((x) => (
+        <div>
+          <h1>{x.name}</h1> <h3>{x.description}</h3>
+        </div>
+      ))}
     </div>
   );
 }
